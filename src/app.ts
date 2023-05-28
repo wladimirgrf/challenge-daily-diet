5844d731443ab5acc8b878e225ddbe725cb79696
wladimirgrf <wladimir.code@gmail.com>
2023-05-28 13:26:17 -0300

    feat: app, server, lib and env instances

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import { ZodError } from 'zod'
+
+import { env } from './env'
+
+export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: { expiresIn: '10m' },
+})
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error!' })
+})
